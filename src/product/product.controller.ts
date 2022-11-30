cb6c91eec3684c6f09373ff2cc71395e4e1b3b32
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-11-30 20:47:39 +0800

    top page service and controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common'
@@ -16,16 +17,19 @@ import { FindProductDto } from './dto/find-product.dto'
 import { CreateProductDto } from './dto/create-product.dto'
 import { ProductService } from './product.service'
 import { IdValidationPipe } from '../pipes/id-validation.pipe'
+import { JwtAuthGuard } from '../auth/guards/jwt.guard'
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('create')
   async create(@Body() dto: CreateProductDto) {
     return this.productService.create(dto)
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async get(@Param('id', IdValidationPipe) id: string) {
     const product = await this.productService.findDyId(id)
@@ -34,12 +38,14 @@ export class ProductController {
     return product
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id', IdValidationPipe) id: string) {
     const deletedProduct = await this.productService.deleteById(id)
     if (!deletedProduct) throw new NotFoundException('Product not found')
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: ProductModel) {
     const updatedProduct = await this.productService.updateById(id, dto)
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -4,40 +4,71 @@ import {
   Delete,
   Get,
   HttpCode,
+  NotFoundException,
   Param,
   Patch,
   Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common'
-import { TopPageModel } from './top-page.model/top-page.model'
 import { FindTopPageDto } from './dto/find-top-page.dto'
+import { TopPageService } from './top-page.service'
+import { CreateTopPageDto } from './dto/create-top-page.dto'
+import { IdValidationPipe } from '../pipes/id-validation.pipe'
+import { JwtAuthGuard } from '../auth/guards/jwt.guard'
 
 @Controller('top-page')
 export class TopPageController {
-  constructor() {}
+  constructor(private readonly topPageService: TopPageService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('create')
-  async create(@Body() dto: Omit<TopPageModel, '_id'>) {
-    console.log()
+  async create(@Body() dto: CreateTopPageDto) {
+    return this.topPageService.create(dto)
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  async get(@Param('id') id: string) {
-    console.log()
+  async get(@Param('id', IdValidationPipe) id: string) {
+    const page = await this.topPageService.findById(id)
+    if (!page) {
+      throw new NotFoundException('Page not found')
+    }
+    return page
   }
 
+  @Get('byAlias/:alias')
+  async getByAlias(@Param('alias') alias: string) {
+    const page = await this.topPageService.findByAlias(alias)
+    if (!page) {
+      throw new NotFoundException('Page not found')
+    }
+    return page
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
-    console.log()
+    const page = await this.topPageService.deleteById(id)
+    if (!page) {
+      throw new NotFoundException('Page not found')
+    }
   }
 
   @Patch(':id')
-  async patch(@Param('id') id: string, @Body() dto: TopPageModel) {
-    console.log()
+  async patch(@Param('id') id: string, @Body() dto: CreateTopPageDto) {
+    const page = await this.topPageService.updateById(id, dto)
+    if (!page) {
+      throw new NotFoundException('Page not found')
+    }
+    return page
   }
 
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
-  @Post()
+  @Post('find')
   async find(@Body() dto: FindTopPageDto) {
-    console.log()
+    return this.topPageService.findByCategory(dto.firstCategory)
   }
 }
